d12c43a10b91613e12af2c7cdbe27685fe8a2b33
Corneille Tshibasu <corneilletshibasu@gmail.com>
2021-07-12 17:27:54 +0100

    ignore .env, update app.js and package.json

---
+++ app.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv')
+const express = require('express')
+const cors = require('cors')
+
+dotenv.config({ path: './config/config.env' })
+
+// start up app obj for express
+const app = express();
+
+// app uses cross-origin
+app.use(cors())
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Success!</h1>');
+    // console.log(process.env.PORT) - works and picks up the right
+});
+
+// port to listen to + app = exp instance
+const PORT = process.env.PORT || 3000
+
+// server to listen on port
+app.listen(PORT)
