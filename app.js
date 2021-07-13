92e7b7caf5de864880dbd473accc7f3ecd3668b4
Corneille Tshibasu <corneilletshibasu@gmail.com>
2021-07-13 16:04:41 +0100

    update code-base

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const dotenv = require('dotenv')
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser');
+const errorController = require('./controllers/error');
+const mongoose = require('mongoose')
+
 
 // configure path for the env variables
 dotenv.config({ path: './config/config.env' })
@@ -10,6 +15,7 @@ const app = express();
 
 // app uses cross-origin
 app.use(cors())
+app.use(express.json())
 
 app.use('/', (req, res, next) => {
     res.send('<h1>Success!</h1>');
@@ -19,5 +25,17 @@ app.use('/', (req, res, next) => {
 // port to listen to + app = exp instance
 const PORT = process.env.PORT || 3000
 
+// error controller if page can't be found
+app.use(errorController.get404);
+
 // server to listen on port
-app.listen(PORT)
+// later using mongoose to connect to the data
+mongoose.
+    connect(process.env.MONGO_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(result => {
+        app.listen(PORT)
+        console.log(`Connected on PORT ${PORT}`)
+    })
+    .catch(err => {
+        console.log(err);
+    });
--- models/villager.js
+++ models/villager.js
@@ -1,34 +1,81 @@
 // import mongoose
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose')
+const mongodb = require('mongodb')
+const getDb = require('../util/database').getDb
+
+// Object to correct id data type from mongodb
+const ObjectID = mongodb.ObjectID
+
+class Villager {
+
+    // ctor function for Villager Object
+    constructor(name, species, birthdayString, personality, catchphrase, hobby, saying, subtype, villagerID, id) {
+        this.name = name
+        this.species = species
+        this.birthdayString = birthdayString
+        this.personality = personality
+        this.catchphrase = catchphrase
+        this.hobby = hobby
+        this.saying = saying
+        this.subtype = subtype
+        this.villagerID = villagerID
+
+        // id
+        this._id = id ? new mongodb.ObjectID(id) : null
+    }
+
+    // function to display the data of the villagers, possible limit the result and make it random selection
+    static fetchVillagers() {
+
+        // use const for connection
+        const db = getDb()
+
+        // return result of the query
+        return db
+            .collection('villagers')
+            .aggregate([{ $sample: { size: 9 }}])
+            .find()
+            .toArray()
+            .then(villagers => {
+                console.log(villagers)
+                return villagers
+            })
+            .catch(err => console.log(err));
+
+    }
+
+}
+
+module.exports = Villager
 
 // create data schema for villager
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
 // initialize schema, holds attributes for villager
 // mirrors the api call data format for villagers
-const villagerSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    species: {
-        type: String,
-        required: true
-    },
-    personality: {
-        type: String,
-        required: true
-    },
-    catchphrase: {
-        type: String,
-        required: true
-    },
-    hobby: {
-        type: String,
-        required: true
-    },
-    saying: {
-        type: String,
-        required: true
-    }
-})
+// const villagerSchema = new Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     species: {
+//         type: String,
+//         required: true
+//     },
+//     personality: {
+//         type: String,
+//         required: true
+//     },
+//     catchphrase: {
+//         type: String,
+//         required: true
+//     },
+//     hobby: {
+//         type: String,
+//         required: true
+//     },
+//     saying: {
+//         type: String,
+//         required: true
+//     }
+// })
